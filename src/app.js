62ece40f6ea6dcc67f27cc50be19495fb4bf32a2
srjp <Official.soumyaranjan.07@gmail.com>
2023-03-19 13:04:53 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -6,16 +6,6 @@ import Footer from "./Components/Footer";
 import { Auth0Provider } from "@auth0/auth0-react";
 const App = () => {
   return (
-    <>
-      <Header />
-      <Body />
-      <Footer />
-    </>
-  );
-};
-
-const FApp = () => {
-  return (
     <Auth0Provider
       domain="dev-gbes8amljl3k4q55.us.auth0.com"
       clientId="qy7z6sEmCj6xhFzfsBifx4bftu7SYU7D"
@@ -23,10 +13,12 @@ const FApp = () => {
         redirect_uri: window.location.origin,
       }}
     >
-      <App />
+      <Header />
+      <Body />
+      <Footer />
     </Auth0Provider>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FApp />);
+root.render(<App />);
